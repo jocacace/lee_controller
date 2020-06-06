f2d118654bbc1df0ccc8e59a89c4a65bc440ed65
jcacace <jonathan.cacace@gmail.com>
2020-06-06 23:36:10 +0200

    plan request

--- src/main.cpp
+++ src/main.cpp
@@ -13,16 +13,69 @@ using namespace std;
 using namespace Eigen;
 
 
+
+
+
 inline Matrix3d QuatToMat(Vector4d Quat){
-		Matrix3d Rot;
-		float s = Quat[0];
-		float x = Quat[1];
-		float y = Quat[2];
-		float z = Quat[3];
-		Rot << 1-2*(y*y+z*z),2*(x*y-s*z),2*(x*z+s*y),
-		2*(x*y+s*z),1-2*(x*x+z*z),2*(y*z-s*x),
-		2*(x*z-s*y),2*(y*z+s*x),1-2*(x*x+y*y);
-		return Rot;
+    Matrix3d Rot;
+    float s = Quat[0];
+    float x = Quat[1];
+    float y = Quat[2];
+    float z = Quat[3];
+    Rot << 1-2*(y*y+z*z),2*(x*y-s*z),2*(x*z+s*y),
+    2*(x*y+s*z),1-2*(x*x+z*z),2*(y*z-s*x),
+    2*(x*z-s*y),2*(y*z+s*x),1-2*(x*x+y*y);
+    return Rot;
+}
+
+
+	inline Vector4d rot2quat(Matrix3d R){
+
+		float m00, m01, m02, m10, m11, m12, m20, m21, m22;
+
+		m00 = R(0,0);
+		m01 = R(0,1);
+		m02 = R(0,2);
+		m10 = R(1,0);
+		m11 = R(1,1);
+		m12 = R(1,2);
+		m20 = R(2,0);
+		m21 = R(2,1);
+		m22 = R(2,2);
+
+		float tr = m00 + m11 + m22;
+		float qw, qx, qy, qz, S;
+		Vector4d quat;
+
+		if (tr > 0) { 
+		  S = sqrt(tr+1.0) * 2; // S=4*qw 
+		  qw = 0.25 * S;
+		  qx = (m21 - m12) / S;
+		  qy = (m02 - m20) / S; 
+		  qz = (m10 - m01) / S; 
+		} else if ((m00 > m11)&(m00 > m22)) { 
+		  S = sqrt(1.0 + m00 - m11 - m22) * 2; // S=4*qx 
+		  qw = (m21 - m12) / S;
+		  qx = 0.25 * S;
+		  qy = (m01 + m10) / S; 
+		  qz = (m02 + m20) / S; 
+		} else if (m11 > m22) { 
+		  S = sqrt(1.0 + m11 - m00 - m22) * 2; // S=4*qy
+		  qw = (m02 - m20) / S;
+		  qx = (m01 + m10) / S; 
+		  qy = 0.25 * S;
+		  qz = (m12 + m21) / S; 
+		} else { 
+		  S = sqrt(1.0 + m22 - m00 - m11) * 2; // S=4*qz
+		  qw = (m10 - m01) / S;
+		  qx = (m02 + m20) / S;
+		  qy = (m12 + m21) / S;
+		  qz = 0.25 * S;
+		}
+
+		quat << qw, qx, qy, qz;
+		return quat;
+
 	}
 	
 	
@@ -57,6 +110,41 @@ inline Vector3d R2XYZ(Matrix3d R) {
     XYZ << phi,theta,psi;
     return XYZ;
 }
+
+ 
+
+	inline Matrix3d XYZ2R(Vector3d angles) {
+	  	
+	  	Matrix3d R = Matrix3d::Zero(); 
+	  	Matrix3d R1 = Matrix3d::Zero(); 
+	  	Matrix3d R2 = Matrix3d::Zero(); 
+	  	Matrix3d R3 = Matrix3d::Zero();
+
+		float cos_phi = cos(angles[0]);
+		float sin_phi = sin(angles[0]);
+		float cos_theta = cos(angles[1]);
+		float sin_theta = sin(angles[1]);
+		float cos_psi = cos(angles[2]);
+		float sin_psi = sin(angles[2]);
+
+		R1  << 1, 0      , 0, 
+			        0, cos_phi, -sin_phi, 
+			        0, sin_phi, cos_phi;
+
+		R2  << cos_theta , 0, sin_theta, 
+			        0        , 1, 0       , 
+			        -sin_theta, 0, cos_theta;
+
+		R3  << cos_psi, -sin_psi, 0, 
+			        sin_psi, cos_psi , 0,
+			        0      , 0       , 1;
+
+		R = R1*R2*R3;
+
+		return R;
+	}
+
+
 class CONTROLLER {
 
     public:
@@ -66,6 +154,7 @@ class CONTROLLER {
         void ctrl_loop();
         bool get_allocation_matrix(Eigen::MatrixXd & allocation_M, int motor_size );
         void ModelStateCb( gazebo_msgs::ModelStates ms );
+        void request_new_plan();
 
     private:
         ros::NodeHandle _nh;
@@ -75,6 +164,9 @@ class CONTROLLER {
         ros::Publisher _cmd_vel_motor_pub;
         nav_msgs::Odometry _odom;
         bool _first_odom;
+        motion_planner::generate_plan _plan;
+        bool _new_plan;
+
 
 };
 
@@ -82,7 +174,7 @@ class CONTROLLER {
 
 
 
-CONTROLLER::CONTROLLER(): _first_odom(false) {
+CONTROLLER::CONTROLLER(): _first_odom(false), _new_plan(false) {
 
     //_odom_sub = _nh.subscribe("/iris/odometry_sensor1/odometry", 0, &CONTROLLER::OdometryCallback, this);
     _model_state_sub = _nh.subscribe("/gazebo/model_states", 0, &CONTROLLER::ModelStateCb, this);
@@ -90,7 +182,52 @@ CONTROLLER::CONTROLLER(): _first_odom(false) {
     _cmd_vel_motor_pub = _nh.advertise<std_msgs::Float32MultiArray>("/iris_smc/cmd/motor_vel", 0);
 }
 
+void CONTROLLER::request_new_plan() {
 
+
+    float x, y, z, yaw;
+    ros::ServiceClient client = _nh.serviceClient<motion_planner::generate_plan>("generate_tarjectory");
+
+    while(ros::ok()) {
+        cout << "Insert new coordinates x (front), y (left), z (upword), yaw (c-clowise)" <<endl;
+        scanf("%f %f %f %f", &x, &y, &z, &yaw);
+
+        cout << "Request new plan for: [" << x << ", " << y << ", " << z << " - " << yaw << "]" << endl;
+
+        _plan.request.p_i.position.x = _odom.pose.pose.position.x;
+        _plan.request.p_i.position.y = _odom.pose.pose.position.y;
+        _plan.request.p_i.position.z = _odom.pose.pose.position.z;
+        Vector3d eu = R2XYZ( QuatToMat ( Vector4d( _odom.pose.pose.orientation.w,  _odom.pose.pose.orientation.x, _odom.pose.pose.orientation.y, _odom.pose.pose.orientation.z ) ) );
+        
+        tf::Quaternion q;
+        q.setRPY(0, 0, eu(2));
+        q = q.normalize();
+
+        _plan.request.p_i.orientation.w = q.w();
+        _plan.request.p_i.orientation.x = q.x();
+        _plan.request.p_i.orientation.y = q.y();
+        _plan.request.p_i.orientation.z = q.z();
+
+
+        _plan.request.p_f.position.x = x;
+        _plan.request.p_f.position.y = y;
+        _plan.request.p_f.position.z = z;
+
+
+        Vector4d dq = rot2quat ( XYZ2R( Vector3d( 0, 0, yaw) ) );
+
+
+        _plan.request.p_f.orientation.w = dq[0];
+        _plan.request.p_f.orientation.x = dq[1];
+        _plan.request.p_f.orientation.y = dq[2];
+        _plan.request.p_f.orientation.z = dq[3];
+
+
+        if( client.call( _plan ) ) {
+          _new_plan = true;
+        }        
+    }
+}
 
 void CONTROLLER::ModelStateCb( gazebo_msgs::ModelStates ms ) {
     
@@ -100,7 +237,8 @@ void CONTROLLER::ModelStateCb( gazebo_msgs::ModelStates ms ) {
 
     nav_msgs::Odometry gazebo_odom;
     while(!found && index < ms.name.size()) {
-        if( ms.name[index] == "iris_smc" ) {
+        if( ms.name[index] == "ir
+        is_smc" ) {
             found = true;
         }
         else index++;
@@ -145,18 +283,18 @@ bool CONTROLLER::get_allocation_matrix(Eigen::MatrixXd & allocation_M, int motor
     double direction = 1.0;
 
     allocation_M(0, i) =  sin( rotor_angle ) * arm_length * force_k;
-    allocation_M(1, i) = -cos( rotor_angle ) * arm_length * force_k;
-    allocation_M(2, i) = -direction * force_k * moment_k;
-    allocation_M(3, i) = force_k;
+    allocation_M(1, i) = cos( rotor_angle ) * arm_length * force_k;
+    allocation_M(2, i) = direction * force_k * moment_k;
+    allocation_M(3, i) = -force_k;
 
     i++;
     rotor_angle = 2.565;
     arm_length = 0.238;
     direction = 1.0;
     allocation_M(0, i) =  sin( rotor_angle ) * arm_length * force_k;
-    allocation_M(1, i) = -cos( rotor_angle ) * arm_length * force_k;
-    allocation_M(2, i) = -direction * force_k * moment_k;
-    allocation_M(3, i) = force_k;
+    allocation_M(1, i) = cos( rotor_angle ) * arm_length * force_k;
+    allocation_M(2, i) = direction * force_k * moment_k;
+    allocation_M(3, i) = -force_k;
 
 
     i++;
@@ -164,9 +302,9 @@ bool CONTROLLER::get_allocation_matrix(Eigen::MatrixXd & allocation_M, int motor
     arm_length = 0.255;
     direction = -1.0;
     allocation_M(0, i) =  sin( rotor_angle ) * arm_length * force_k;
-    allocation_M(1, i) = -cos( rotor_angle ) * arm_length * force_k;
-    allocation_M(2, i) = -direction * force_k * moment_k;
-    allocation_M(3, i) = force_k;
+    allocation_M(1, i) = cos( rotor_angle ) * arm_length * force_k;
+    allocation_M(2, i) = direction * force_k * moment_k;
+    allocation_M(3, i) = -force_k;
 
 
     i++; 
@@ -174,9 +312,9 @@ bool CONTROLLER::get_allocation_matrix(Eigen::MatrixXd & allocation_M, int motor
     arm_length = 0.238;
     direction = -1.0;
     allocation_M(0, i) =  sin( rotor_angle ) * arm_length * force_k;
-    allocation_M(1, i) = -cos( rotor_angle ) * arm_length * force_k;
-    allocation_M(2, i) = -direction * force_k * moment_k;
-    allocation_M(3, i) = force_k;
+    allocation_M(1, i) = cos( rotor_angle ) * arm_length * force_k;
+    allocation_M(2, i) = direction * force_k * moment_k;
+    allocation_M(3, i) = -force_k;
     /*
     for(int i=0; i<_motor_size; i++) {
 
@@ -241,7 +379,7 @@ void CONTROLLER::ctrl_loop() {
   Eigen::Vector3d des_dp; 
   Eigen::Vector3d des_ddp; 
 
-  des_p << 0.0, 0.0, 1.0;
+  des_p << 0.0, 0.0, 0.0;
   des_dp << 0.0, 0.0, 0.0;
   des_ddp << 0.0, 0.0, 0.0;
   
@@ -287,13 +425,17 @@ void CONTROLLER::ctrl_loop() {
 
 
   while( !_first_odom ) usleep(0.1*1e6);
+  boost::thread input_t( &CONTROLLER::request_new_plan, this);
 
-  Vector3d eu = R2XYZ( QuatToMat ( Vector4d( _odom.pose.pose.orientation.w,  _odom.pose.pose.orientation.x, _odom.pose.pose.orientation.y, _odom.pose.pose.orientation.z ) ) );
 
-  double des_yaw = eu(2);
+
+
   if( !get_allocation_matrix( allocation_M, _motor_num ) ) exit(0);
   else cout << "Matrix: " << allocation_M << endl;
   //exit(0);
+
+  Vector3d eu = R2XYZ( QuatToMat ( Vector4d( _odom.pose.pose.orientation.w,  _odom.pose.pose.orientation.x, _odom.pose.pose.orientation.y, _odom.pose.pose.orientation.z ) ) );
+  double des_yaw = eu(2);
 
   wd2rpm.resize( _motor_num, 4 );
   //Malloc dagger
@@ -311,54 +453,38 @@ void CONTROLLER::ctrl_loop() {
   motor_vel.data.resize(4);
 
 
-    
-  ros::ServiceClient client = _nh.serviceClient<motion_planner::generate_plan>("generate_tarjectory");
-  motion_planner::generate_plan plan;
-  
-  
-    plan.request.p_i.position.x = _odom.pose.pose.position.x;
-    plan.request.p_i.position.y = _odom.pose.pose.position.y;
-    plan.request.p_i.position.z = _odom.pose.pose.position.z;
-
-    tf::Quaternion q;
-    q.setRPY(0, 0, eu(2));
-    q = q.normalize();
-
-    plan.request.p_i.orientation.w = q.w();
-    plan.request.p_i.orientation.x = q.x();
-    plan.request.p_i.orientation.y = q.y();
-    plan.request.p_i.orientation.z = q.z();
-
-
-    plan.request.p_f.position.x = 2; //_odom.pose.pose.position.x;
-    plan.request.p_f.position.y = _odom.pose.pose.position.y;
-    plan.request.p_f.position.z = 2; //_odom.pose.pose.position.z;
-
-
-    plan.request.p_f.orientation.w = 1;
-    plan.request.p_f.orientation.x = 0.0;
-    plan.request.p_f.orientation.y = 0.0;
-    plan.request.p_f.orientation.z = 0.0;
-  
-
-    if( client.call( plan ) ) {
-        cout << "Planned!" << endl;
-    }
-    else {
-        exit(0);
-    }
- // while( ros::ok() ) {
+  int plan_index = 0;
+   
+    while( ros::ok() ) {
 
       
 
     Eigen::VectorXd ref_rotor_velocities;
     //lee_position_controller_.CalculateRotorVelocities(&ref_rotor_velocities);
 
+    /*
     for(int i=0; i<plan.response.traj.points[0].positions.size(); i++ ) {
 
         des_p << plan.response.traj.points[0].positions[i],  plan.response.traj.points[1].positions[i], plan.response.traj.points[2].positions[i];
         des_yaw = plan.response.traj.points[3].positions[i];
+    */
 
+    if( _new_plan ) {
+        _new_plan = false;
+        plan_index = 0;
+    }
+
+    if( _plan.response.traj.points.size() > 0.0 ) {
+        if( plan_index < _plan.response.traj.points[0].positions.size()-1 ) {
+            plan_index++;
+            des_p << _plan.response.traj.points[0].positions[plan_index], _plan.response.traj.points[1].positions[plan_index], _plan.response.traj.points[2].positions[plan_index];
+            des_dp << _plan.response.traj.points[0].velocities[plan_index], _plan.response.traj.points[1].velocities[plan_index], _plan.response.traj.points[2].velocities[plan_index];
+            des_ddp << _plan.response.traj.points[0].accelerations[plan_index], _plan.response.traj.points[1].accelerations[plan_index], _plan.response.traj.points[2].accelerations[plan_index];
+         
+            des_yaw = _plan.response.traj.points[3].positions[plan_index];
+        }
+
+    }
 
 
 
@@ -366,7 +492,10 @@ void CONTROLLER::ctrl_loop() {
     mes_dp << _odom.twist.twist.linear.x, _odom.twist.twist.linear.y, _odom.twist.twist.linear.z; 
     mes_q = Eigen::Quaterniond( _odom.pose.pose.orientation.w, _odom.pose.pose.orientation.x, _odom.pose.pose.orientation.y, _odom.pose.pose.orientation.z );
     mes_w << _odom.twist.twist.angular.x, _odom.twist.twist.angular.y, _odom.twist.twist.angular.z;  
-    
+
+    Eigen::Matrix3d R = mes_q.toRotationMatrix();
+    mes_w = R.transpose()*mes_w;
+
     lc.controller( _motor_num, mes_p, des_p, mes_q, mes_dp, des_dp, des_ddp, des_yaw, mes_w,
                                             position_gain, velocity_gain, normalized_attitude_gain, normalized_angular_rate_gain, wd2rpm, mass, gravity, &ref_rotor_velocities);
     
@@ -386,9 +515,9 @@ void CONTROLLER::ctrl_loop() {
     _cmd_vel_motor_pub.publish( motor_vel );
 
     r.sleep();
-   }
+  // }
 
-//  }
+  }
 
 
 }
